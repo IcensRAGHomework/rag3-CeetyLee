cc47607d090e2638ea083f697aae8c9ed4638cb5
ceety_lee <ceety_lee@asus.com>
2025-03-07 16:48:52 +0800

    for hw3-3

--- student_assignment.py
+++ student_assignment.py
@@ -50,18 +50,22 @@ def get_collection():
             )
     return collection
 
-def get_query_results(collection, question, city, store_type, start_date, end_date, store_name, new_store_name):
+def get_query_results(collection, question, city, store_type, start_date, end_date):
+    filters = [
+        {"type": {"$in": store_type}},
+        {"city": {"$in": city}}
+    ]
+    if start_date:
+        filters.append({"date": {"$gte": int(start_date.timestamp())}}) # greater than or equal
+    if end_date:
+        filters.append({"date": {"$lte": int(end_date.timestamp())}}) # less than or equal
+
     query_results = collection.query(
         query_texts = [question],
         n_results = 10,
         include = ["metadatas", "distances"],
         where = {
-            "$and": [
-                {"date": {"$gte": int(start_date.timestamp())}}, # greater than or equal
-                {"date": {"$lte": int(end_date.timestamp())}}, # less than or equal
-                {"type": {"$in": store_type}},
-                {"city": {"$in": city}}
-            ]
+            "$and": filters
         }
     )
     return query_results
@@ -73,15 +77,24 @@ def generate_hw01():
     
 def generate_hw02(question, city, store_type, start_date, end_date):
     collection = get_collection()
-    query_results = get_query_results(collection, question, city, store_type, start_date, end_date, store_name=None, new_store_name=None)
-    names, distances = query_results.get("metadatas",[[]])[0], query_results.get("distances", [[]])[0]
+    hw02_query_results = get_query_results(collection, question, city, store_type, start_date, end_date)
+    names, distances = hw02_query_results.get("metadatas",[[]])[0], hw02_query_results.get("distances", [[]])[0]
     sorted_results = [x.get("name") for x, d in sorted(zip(names, distances), key=lambda x: x[1]) if d < 0.2]
-    print(sorted_results)
+    #print(sorted_results)
     return sorted_results
 
     
 def generate_hw03(question, store_name, new_store_name, city, store_type):
-    pass
+    collection = get_collection()
+    selected = collection.get(where = {"name": store_name})
+    metadatas = [{**item, "new_store_name": new_store_name} for item in selected.get("metadatas", [])]
+    collection.upsert(ids = selected.get("ids", []), metadatas = metadatas, documents = selected.get("documents", []))
+    hw03_query_results = get_query_results(collection, question, city, store_type, start_date = None, end_date = None)
+    names, distances = hw03_query_results.get("metadatas", [[]])[0], hw03_query_results.get("distances", [[]])[0]
+    sorted_results = [x.get("new_store_name", x.get("name")) for x, d in sorted(zip(names, distances), key=lambda x: x[1]) if d < 0.2]
+    #print(sorted_results)
+    return sorted_results
+
     
 def demo(question):
     chroma_client = chromadb.PersistentClient(path=dbpath)
@@ -100,8 +113,11 @@ def demo(question):
 
     return collection
 
-print("-----hw3-1")
-generate_hw01()
-print("\n")
-print("-----hw3-2")
-generate_hw02("我想要找有關茶餐點的店家", ["宜蘭縣", "新北市"], ["美食"], datetime.datetime(2024, 4, 1), datetime.datetime(2024, 5, 1))
+# print("-----hw3-1")
+# generate_hw01()
+# print("\n")
+# print("-----hw3-2")
+# generate_hw02("我想要找有關茶餐點的店家", ["宜蘭縣", "新北市"], ["美食"], datetime.datetime(2024, 4, 1), datetime.datetime(2024, 5, 1))
+# print("\n")
+# print("-----hw3-3")
+# generate_hw03("我想要找南投縣的田媽媽餐廳，招牌是蕎麥麵", "耄饕客棧", "田媽媽（耄饕客棧）", ["南投縣"], ["美食"])
